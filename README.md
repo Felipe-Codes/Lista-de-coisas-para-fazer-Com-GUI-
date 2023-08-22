c2dd4ff4c07361d90d44799ec94f2c4193c0c37a
Felipe-Codes <124637823+Felipe-Codes@users.noreply.github.com>
2023-08-21 22:31:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lista-de-coisas-para-fazer-Com-GUI-
+ Uma programa que adiciona uma tarefa na lista e você pode gerenciar
---
+++ main.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+
+def add_task():
+    task = entry.get()
+    if task:
+        listbox.insert(tk.END, task)
+        entry.delete(0, tk.END)
+
+def remove_task():
+    selected_task_index = listbox.curselection()
+    if selected_task_index:
+        listbox.delete(selected_task_index)
+
+root = tk.Tk()
+root.title("Fazer")
+
+entry = tk.Entry(root, width=40)
+entry.pack(pady=10)
+
+add_button = tk.Button(root, text="Add Task", command=add_task)
+add_button.pack()
+
+remove_button = tk.Button(root, text="Remove Task", command=remove_task)
+remove_button.pack()
+
+listbox = tk.Listbox(root, width=40)
+listbox.pack()
+
+root.mainloop()
